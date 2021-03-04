f7554d51a719516b425f4f96ee8f411e3473bbb5
unimatrix2 <dnny.nis@gmail.com>
2021-03-04 15:29:45 -0300

    added board model

---
+++ src/models/Board.model.js
@@ -0,0 +1,10 @@
+import { Schema, model } from 'mongoose';
+
+const boardSchema = new Schema({
+    abbr: { type: String, required: true, max: 10 },
+    description: { type: String, required: true, max: 400 },
+    rules: [{ type: String, required: true }],
+    creator: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+}, { timestamps: true });
+
+export const Board = model('Board', boardSchema);
