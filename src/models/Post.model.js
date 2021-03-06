6f46fefeddc9e6f4a943c7d2dcf4641f7ca96f66
unimatrix2 <dnny.nis@gmail.com>
2021-03-06 01:55:40 -0300

    added post model; image storage is pending

--- src/models/Post.model.js
+++ src/models/Post.model.js
@@ -2,6 +2,9 @@ import { Schema, model } from 'mongoose';
 
 const postSchema = new Schema({
     image_file: {  },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    board: { type: Schema.Types.ObjectId, ref: 'Board', required: true },
+    content: { type: String }
 }, { timestamps: true });
 
 export const Post = model('Post', postSchema);
