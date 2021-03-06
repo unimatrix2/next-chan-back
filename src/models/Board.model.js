aa4c5819ea9ef9b958b40497f69a7866fbf09cd2
unimatrix2 <dnny.nis@gmail.com>
2021-03-06 01:55:11 -0300

    added modmins to model

--- src/models/Board.model.js
+++ src/models/Board.model.js
@@ -4,7 +4,8 @@ const boardSchema = new Schema({
     abbr: { type: String, required: true, max: 10 },
     description: { type: String, required: true, max: 400 },
     rules: [{ type: String, required: true }],
-    creator: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+    creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    modmins: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }]
 }, { timestamps: true });
 
 export const Board = model('Board', boardSchema);
