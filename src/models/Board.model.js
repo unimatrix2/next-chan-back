a5821c1f508661f7f12acb3b025ab0449cadf8e8
unimatrix2 <dnny.nis@gmail.com>
2021-03-12 23:47:04 -0300

    fixed field, added title & secret for 1+ modmins

--- src/models/Board.model.js
+++ src/models/Board.model.js
@@ -2,10 +2,12 @@ import { Schema, model } from 'mongoose';
 
 const boardSchema = new Schema({
     abbr: { type: String, required: true, max: 10 },
+    title: { type: String, required: true, max: 40 },
     description: { type: String, required: true, max: 400 },
     rules: [{ type: String, required: true }],
-    creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    modmins: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }]
+    founder: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    modmins: [{ type: Schema.Types.ObjectId, ref: 'User', required: true }],
+    secret: { type: String, required: true },
 }, { timestamps: true });
 
 export const Board = model('Board', boardSchema);
